0c1d5eca3eca335a2f2820185427dc89d77c21e6
basmundhopk <basmundhopk@alumni.unav.es>
2023-11-09 23:26:02 +0100

    contractor
    with my guy contractor my man

--- contractor_simple.py
+++ contractor_simple.py
@@ -1,18 +1,14 @@
 import openai
 
 openai.api_key = "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"
-file1 = open('guide_rules.txt')
+
+file1 = open('contractor_rules.txt')
 file2 = open('game_state.txt')
-
-
-
-guide_rules = file1.read()
+contractor_rules = file1.read()
 game_state = file2.read()
 
-
-
 messages = []
-messages.append({"role": "system", "content": guide_rules})
+messages.append({"role": "system", "content": contractor_rules})
 messages.append({"role": "user", "content": game_state})
 completion = openai.ChatCompletion.create( 
     model="gpt-3.5-turbo",
