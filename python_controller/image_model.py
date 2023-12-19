a8dfb889706f20b56db32d6dca8641c67ad5a5e8
csherf <chadnsherf@gmail.com>
2023-12-19 15:21:40 +0100

    updated label format

--- python_controller/image_model.py
+++ python_controller/image_model.py
@@ -1,12 +1,16 @@
 from ultralytics import YOLO
 
-LABELS = {0: 'card_ability', 1: 'cost', 2: 'crew', 3: 'damage_1', 4: 'damage_2', 5: 'damage_3', 6: 'miss', 7: 'name', 8: 'reactor', 9: 'shield', 10: 'shippart_name', 11: 'thruster', 12: 'type'}
-
+LABELS = {
+            0: 'card_ability', 1: 'cost', 2: 'crew', 
+            3: 'damage_1', 4: 'damage_2', 5: 'damage_3', 
+            6: 'miss', 7: 'name', 8: 'reactor', 
+            9: 'shield', 10: 'shippart_name', 
+            11: 'thruster', 12: 'type'
+        }
 
 class IMGmodel:
     def __init__(self):
         pass
-
 
     def predict():
         model = YOLO('weights/best.pt')
